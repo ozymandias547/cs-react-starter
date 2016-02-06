430db54193ce9197c0f44a9a469a32190b0d8385
Joseph Jung <jungyue@gmail.com>
2016-02-06 16:37:34 -0600

    Initial commit for react router.

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,21 +1,44 @@
 var domready = require('domready');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var Link = require('react-router').Link;
+var browserHistory = require('react-router').browserHistory;
+
+
 require('./scss/base.scss');
 
 domready(function () {
   
-	var App = React.createClass({
-		render: function() {
-			return (
-				<div>
-					hi
-				</div>
-			)
-		}
-	})
+	ReactDOM.render(
+		(
+	  		<Router history={browserHistory}>
+	    		<Route path="/" component={Home}></Route>
+	    		<Route path="/page2" component={Page2}></Route>
+	  		</Router>
+		), 
+	document.querySelector(".reactApp"))
 
-	ReactDOM.render(<App />, document.querySelector(".reactApp"));
 
 })
 
+var Home = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<Link to={`/page2`}>Page2</Link>
+			</div>
+		)
+	}
+})
+
+var Page2 = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<Link to={`/`}>Home</Link>
+			</div>
+		)
+	}
+})
